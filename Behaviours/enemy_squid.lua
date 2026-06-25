b39d869a73933d181da08e14269881d9e3e93092
MAKYUNI12 <makyunikoomori@gmail.com>
2026-06-24 23:53:29 -0300

    Lua Squid (WIP)
    Based on Psychosquid's cuttlefish.
    I can't get the Y movement / focus to work.

--- Behaviours/enemy_squid.lua
+++ Behaviours/enemy_squid.lua
@@ -2,29 +2,38 @@ local mx = -3
 local my = 0
 local dx = 0
 local dy = 0
-local target = nil
 local length
 local speed = 6
 local timer
 local fireTimer = 100
-local targetTimer
 local leaveTimer = 1200
-local knockback = 0
 local finSprite
 local finCollider = nil
 local t = 1
 
+local posY = 0
+
+local focusx = 0
+local focusy = 0
+
 function OnInitialise()
+
+    focusx = self.commandArgs.GetFieldInt("focus_x")
+    focusy = self.commandArgs.GetFieldInt("focus_y")
+
     timer = math.random(0, 30)
-    targetTimer = math.random(50, 200)
 
     finSprite = self.SpawnAttachedSpriteAnimator("Sprites/Enemies/squid bottom", -1)
-    finSprite.position = { x = 20, y = 3 }
+    finSprite.position = { x = 1.5, y = -37.5 }
 
     if finSprite ~= nil then finCollider = finSprite.AddCollider(); finCollider.SetLogicLayerEnemy() end
+
 end
 
 function OnTick()
+
+    -- ANIMATION
+
     finSprite.AnimateToNextFrame(true)
 
     local lastFrame = self.animator.currentFrame
@@ -33,39 +42,51 @@ function OnTick()
 
     if timer > 0 then timer = timer - 1 else t = t + 0.05 end
     
-    if target == nil then target = GetRandomActivePlayer() elseif target ~= nil and not target.isActive then target = nil end
-    if target ~= nil then
-        dx = target.worldPosition.x - self.worldPosition.x
-        dy = target.worldPosition.y - self.worldPosition.y
+
+
+    local posY = self.position.y
+
+    -- MOVEMENT
+
+        dx = focusx --
+        dy = focusy --
         length = math.sqrt(dx * dx + dy * dy)
-        dx = dx / length
-        dy = dy / length
-        my = dy
-    end
+        --dx = dx / length
+        --dy = dy / length
+        --my = dy
+
 
     if leaveTimer > 0 then
         leaveTimer = leaveTimer - 1
-        if dx > 0 or length < 200 then
-            if self.position.x < 700 then mx = mx + 0.2 else mx = mx - 0.2 end
-        else mx = mx - 0.2 end
-    else mx = mx - 0.2 end
-    if mx < -3 then mx = -3 elseif mx > 5 then mx = 5 end
-    if knockback > 0 then knockback = knockback - 0.1 else knockback = 0 end
 
-    self.movement = { x = mx + knockback, y = (my + (math.sin(t) * 0.3 )) * speed, z = 0 }
+        --if dx > 0 or length < 200 then
+        if self.position.x > focusx then mx = mx - 0.2 else mx = mx + 0.2 end
+        if self.position.y > focusy then --my = my - 0.2 else my = my + 0.2 end
+        self.position.y = posY - 0.2 else
+        self.position.y = posY + 0.2 end
+        --else mx = mx - 0.2 end
+
+      
+
+
+    else mx = mx - 0.2 end
+
+
+
+    if mx < -3 then mx = -3 elseif mx > 5 then mx = 5 end
+    --if my < -3 then my = -3 elseif my > 5 then my = 5 end
+
+    
+
+    self.movement = { x = mx, y = my, z = 0 }
 
     if fireTimer > 0 then fireTimer = fireTimer - 1 end
-    if CanFire() then
-        if targetTimer > 0 then targetTimer = targetTimer - 1 else targetTimer = math.random(50, 200); target = nil end
-    end
 
     if self.position.x < -150 then self.Deactivate() end
+
 end
 
 function OnHitByBullet()
-    if self.position.x <= 700 then
-        knockback = 4
-    end
 end
 
 function OnKill()
