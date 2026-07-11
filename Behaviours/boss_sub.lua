f1ad2b881617579b0ad6164e6b29dd2b26b62ecb
Psychosquid <psychohovership@gmail.com>
2026-07-11 15:02:11 +0800

    Added the Sub (Phase 1 only)
    Phase 2 coming soon...

---
+++ Behaviours/boss_sub.lua
@@ -0,0 +1,210 @@
+local mx
+local my
+local targetY
+local currentFrame = 0
+local recoil = 0
+local oktofire = false
+local deathFallbackTick = 0
+local wakeAnimator
+local barrelAnimator
+local barrelOffX
+local barrelOffY
+local fireSFX
+local missileOffX
+local missileOffY
+local missileSFX
+local subOffX
+local subOffY
+local subEntity
+
+function OnInitialise()
+	mx = self.commandArgs.GetFieldFloat("mx", 1)
+    my = self.position.y
+	targetY = self.commandArgs.GetFieldFloat("targetY", self.position.y)
+	wakeAnimator = self.SpawnAttachedSpriteAnimator("Effects/Water/boat wake", 1)
+	barrelAnimator = self.SpawnAttachedSpriteAnimator("Sprites/Boss 1/big barrel", -1)
+	barrelOffX = self.customBehaviourData.GetFieldFloat("barrelOffX", 0)
+	barrelOffY = self.customBehaviourData.GetFieldFloat("barrelOffY", 0)
+	fireSFX = self.customBehaviourData.GetFieldString("fireSFX", "")
+	missileOffX = self.customBehaviourData.GetFieldFloat("missileOffX", 0)
+	missileOffY = self.customBehaviourData.GetFieldFloat("missileOffY", 0)
+	missileSFX = self.customBehaviourData.GetFieldString("missileSFX", "")
+	subOffX = self.customBehaviourData.GetFieldFloat("subOffX", 0)
+	subOffY = self.customBehaviourData.GetFieldFloat("subOffY", 0)
+	subEntity = self.customBehaviourData.GetFieldString("subEntity", "")
+	if IsOriginalVersion() then CreateTurret("legacyTurret", 5, 95, self, Globals.firewait) else
+        if Globals.difficulty <= GameDifficulty.Medium then
+            CreateTurret("turretNastySingle", 25, -70, self, Globals.firewait)
+        else CreateTurret("turretTripleSmall", 25, -70, self, Globals.firewait) end
+    end
+end
+
+function OnTick()
+    UpdateWake()
+	barrelAnimator.position = { x = barrelOffX, y = barrelOffY - recoil }
+    self.movement = { x = mx, y = my - self.position.y, z = 0 }
+	if self.position.x > 420 then mx = mx - 0.01 end
+	if self.position.x < 220 then mx = mx + 0.01 end
+	if self.lifetime % 3 ~= 0 then
+        if targetY < -448 and self.lifetime >= 600 then targetY = targetY + 1
+		elseif targetY < -652 then targetY = targetY + 1 end
+    end
+	if targetY < -640 and self.data.maxHitPoints - self.hitPoints > 500 then self.hitPoints = self.data.maxHitPoints - 500 end
+	my = targetY - (5 + math.cos(math.rad(Globals.levelLifetime * 2 % 360)) * 5)
+	if recoil > 0 then recoil = recoil - 1 end
+	if self.hitPoints > 0 then
+        if targetY >= -448 then oktofire = true end
+		if oktofire then
+			local missileWait = (Globals.difficulty <= GameDifficulty.Medium) and 260 or 200
+			if self.lifetime % 35 == 0 and self.lifetime % 350 > missileWait then
+                local missileArgs = NewJSONObject()
+                missileArgs.AddFieldInt("homingDelay", 30)
+                missileArgs.AddFieldInt("currentAngle", -80)
+                missileArgs.AddFieldInt("var5", math.random(0, 360))
+				SpawnEntityWorld("homingMissile", { x = self.worldPosition.x + missileOffX, y = self.worldPosition.y + missileOffY }, missileArgs)
+                if missileSFX ~= "" then PlaySound(missileSFX) end
+            end
+			if Globals.difficulty > GameDifficulty.Easy then
+			    if self.lifetime % 350 == 100 or self.lifetime % 350 == 200 then
+				    recoil = 20
+					for i = 45, 135, 15 do
+						CreateBullet("enemyshot_car_cannon", self.worldPosition.x + (barrelOffX - 8), self.worldPosition.y + (barrelOffY - 10), math.cos(math.rad(i)) * 5 + mx, math.sin(math.rad(i)) * 7.5)
+						if fireSFX ~= "" then PlaySound(fireSFX) end
+                    end
+                end
+				if self.lifetime % 350 == 150 then
+					recoil = 40
+					for j = 45, 135, 15 do
+                        CreateBullet("enemyshot_car_cannon", self.worldPosition.x + (barrelOffX - 8), self.worldPosition.y + (barrelOffY - 10), math.cos(math.rad(j)) * 4 + mx, math.sin(math.rad(j)) * 11)
+						if fireSFX ~= "" then PlaySound(fireSFX) end
+                    end
+				end
+			else
+			    if self.lifetime % 350 == 100 then
+                    recoil = 40
+					for k = 45, 135, 15 do
+                        CreateBullet("enemyshot_car_cannon", self.worldPosition.x + (barrelOffX - 8), self.worldPosition.y + (barrelOffY - 10), math.cos(math.rad(k)) * 5 + mx, math.sin(math.rad(k)) * 9)
+						if fireSFX ~= "" then PlaySound(fireSFX) end
+                    end
+				end
+				if self.lifetime % 350 == 200 and Globals.difficulty == GameDifficulty.Easy then
+				    recoil = 40
+					for l = 45, 135, 15 do
+                        CreateBullet("enemyshot_car_cannon", self.worldPosition.x + (barrelOffX - 8), self.worldPosition.y + (barrelOffY - 10), math.cos(math.rad(l)) * 5 + mx, math.sin(math.rad(l)) * 10)
+						if fireSFX ~= "" then PlaySound(fireSFX) end
+                    end
+				end
+			end
+		end
+		local oldFrame = currentFrame
+		currentFrame = self.GetDamageFrame(self.hitPoints)
+		self.HandleDamageEffects(currentFrame, oldFrame)
+		self.animator.GoTo(currentFrame)
+		if oldFrame ~= currentFrame then CreateExplosionSquare(self.worldPosition.x + -200, self.worldPosition.y - 40, 400, 80) end
+    end
+	if self.hitPoints > -200 and self.hitPoints <= 0 then
+		if self.data.endKillTimerOnDeath then self.EndKillTimer() end
+		oktofire = false
+		self.hitPoints = self.hitPoints - 1
+		if self.lifetime % 10 == 0 and math.random(-200, 0) > self.hitPoints then
+		    SpawnEntityWorld("explosionMedium", { x = self.worldPosition.x + math.random(-200, 200), y = self.worldPosition.y + math.random(-40, 40) })
+		    SpawnEntityWorld("explosionMedium", { x = self.worldPosition.x + math.random(-200, 200), y = self.worldPosition.y + math.random(-40, 40) })
+        end
+		if self.hitPoints == -200 and self.worldPosition.x > 500 and deathFallbackTick < 300 then
+            deathFallbackTick = deathFallbackTick + 1
+			self.hitPoints = -199
+        end
+	end
+	if self.hitPoints <= -200 then self.Kill() end
+end
+
+function UpdateWake()
+	local text = ""
+	local offset = 0
+	if self.position.y >= -701 then
+	    if self.position.y < -677 then
+            text = "Effects/Water/water spray"
+			offset = -10
+		elseif self.position.y < -651 then
+		    text = "Effects/Water/water spray"
+		    offset = -30
+		elseif self.position.y < -579 then
+		    text = "Effects/Water/boat wake"
+			offset = -30
+		elseif self.position.y < -557 then
+		    text = "Effects/Water/boat wake 2"
+		    offset = -50
+		elseif self.position.y < -521 then
+            text = "Effects/Water/boat wake 3"
+		    offset = 120
+        elseif self.position.y < -471 then
+			text = "Effects/Water/boat wake 3"
+			offset = -80
+		else
+			text = "Effects/Water/boat wake 3"
+			offset = -40
+        end
+    end
+	if text ~= "" and wakeAnimator.currentSheet ~= text then
+		wakeAnimator.Initialise(text, 1)
+		wakeAnimator.ApplyLayerMaterial(self.layer)
+    end
+	wakeAnimator.position = { x = offset, y = Globals.horizonLevels[self.layer] - self.worldPosition.y }
+end
+
+function HasCollision()
+	return self.hitPoints > 0
+end
+
+function IsKilledManually()
+	return true
+end
+
+function OnHitByBullet(bulletEntity)
+	if self.lifetime < 1200 then self.hitPoints = self.hitPoints + (bulletEntity.BulletConsume(self) * 0.7) end
+	self.hitPoints = math.max(self.hitPoints, 0)
+end
+
+function OnHitByPlayer(player)
+	self.hitPoints = math.max(self.hitPoints, 0)
+end
+
+function ShouldKillPlayerOnTouch()
+	return self.lifetime > 70
+end
+
+function OnKill()
+    local subArgs = NewJSONObject()
+    subArgs.AddFieldInt("targetX", 600)
+    subArgs.AddFieldInt("targetY", 300)
+	SpawnEntityWorld(subEntity, { x = self.worldPosition.x + subOffX, y = self.worldPosition.y + subOffY }, subArgs);
+	SpawnEntityWorld("explosionBig", { x = self.worldPosition.x - 150, y = self.worldPosition.y - 50 })
+	SpawnEntityWorld("explosionBig", { x = self.worldPosition.x +  50, y = self.worldPosition.y - 50 })
+	SpawnEntityWorld("explosionBig", { x = self.worldPosition.x -  50, y = self.worldPosition.y + 50 })
+	SpawnEntityWorld("explosionBig", { x = self.worldPosition.x + 150, y = self.worldPosition.y + 50 })
+	self.SpawnShipShards(160, -14, 8, -22, 5, 0, 40, 2, 6, 2, 6)
+    self.SpawnShipDebris(40, -24, 16, -44, 10, 0, 40, 2, 6, 2, 6)
+end
+
+function CanFire()
+	return oktofire
+end
+
+function CreateBullet(entity, _x, _y, _mx, _my)
+    local args = NewJSONObject()
+    args.AddFieldFloat("mx", _mx)
+    args.AddFieldFloat("my", _my)
+
+    SpawnEntityWorld(entity, {x=_x, y=_y}, args)
+end
+
+function CreateExplosionSquare(x, y, width, height)
+	local ny = y + height + 50
+	local nx = x + width - 50
+	local p = 80
+	for ox = x, nx, p do
+		for oy = ny, y, p do
+			SpawnEntityWorld("explosionMedium", { x = ox + RandRangeF(0, 50), y = oy + RandRangeF(0, 50) })
+        end
+    end
+end
